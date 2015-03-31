4954dffd3a4a6d40bc0da124939d8e94f4c91532
O. Thebe <othebe@gmail.com>
2015-03-30 23:51:54 -0700

    Adding typescript definitions.

---
+++ snabric.d.ts
@@ -0,0 +1,18 @@
+﻿// Typescript definitions for Snabric.js
+
+declare var Snabric: {
+    new (element: HTMLCanvasElement): Snabric.ISnabric;
+}
+
+declare module Snabric {
+    export interface ISnabric {
+        getCanvas(): fabric.ICanvas;
+        getFImg(sImg: IImage): fabric.IImage;
+        loadFromUrl(url: string, onLoad: (IImage) => void): void;
+    }
+
+    export interface IImage {
+        getSnap(): Snap.Paper;
+        updateSnap(paper: Snap.Paper): void;
+    }
+} 
